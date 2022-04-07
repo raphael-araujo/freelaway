75f6019531b167098f5f308fcd206ac5938c42ab
Raphael da Silva Araújo <raphaelaraujotoc@outlook.com>
2022-04-07 00:11:49 -0300

    adicionando as views para cadastro e login

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def cadastro(request):
+    """view function para a página de cadastro"""
+    return render(request, 'cadastro.html')
+
+def logar(request):
+    return HttpResponse('Logar')
