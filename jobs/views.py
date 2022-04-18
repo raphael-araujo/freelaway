0ac373736dfe655484f26b88d29c331d869030e9
Raphael da Silva Araújo <raphaelaraujotoc@outlook.com>
2022-04-17 23:29:24 -0300

    adicionando a view function para enviar projeto

--- jobs/views.py
+++ jobs/views.py
@@ -1,14 +1,17 @@
 from datetime import datetime
-from django.contrib.auth.models import User
+
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.contrib.messages import constants
-from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
+
 from .models import Job
 
 # Create your views here.
 
 
+@login_required(login_url='/auth/logar')
 def encontrar_jobs(request):
     if request.method == 'GET':
         preco_minimo = request.GET.get('preco_minimo')
@@ -48,6 +51,7 @@ def encontrar_jobs(request):
         return render(request, 'encontrar_jobs.html', {'jobs': jobs})
 
 
+@login_required(login_url='/auth/logar')
 def aceitar_job(request, id):
     job = get_object_or_404(Job, id=id)
     job.profissional = request.user
@@ -57,6 +61,7 @@ def aceitar_job(request, id):
     return redirect('/jobs/encontrar_jobs/')
 
 
+@login_required(login_url='/auth/logar')
 def perfil(request):
     if request.method == 'GET':
         jobs = Job.objects.filter(profissional=request.user)
@@ -86,3 +91,17 @@ def perfil(request):
         messages.add_message(request, constants.SUCCESS,
                              'Dados alterados com sucesso.')
         return redirect('/jobs/perfil/')
+
+
+@login_required(login_url='/auth/logar')
+def enviar_projeto(request):
+    arquivo = request.FILES.get('file')
+    id_job = request.POST['id']
+
+    job = Job.objects.get(id=id_job)
+
+    job.arquivo_final = arquivo
+    job.status = 'AA'
+    job.save()
+
+    return redirect('/jobs/perfil/')
